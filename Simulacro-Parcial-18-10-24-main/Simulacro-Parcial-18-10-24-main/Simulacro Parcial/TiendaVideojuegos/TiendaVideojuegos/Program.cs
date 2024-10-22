83fe80b11f5bdc77107d301089936efb69f697f8
LenaAlbi3 <115093034+LenaAlbi3@users.noreply.github.com>
2024-10-22 16:38:37 -0300

    Create Program.cs

---
+++ Simulacro-Parcial-18-10-24-main/Simulacro-Parcial-18-10-24-main/Simulacro Parcial/TiendaVideojuegos/TiendaVideojuegos/Program.cs
@@ -0,0 +1,13 @@
+﻿using TiendaVideojuegos.Modelos;
+
+namespace TiendaVideojuegos
+{
+    public class Program
+    {
+        static void Main()
+        {
+            GestorVideojuegos.CargarDatos(GestorVideojuegos.ArchivoVideojuegos, "+");
+            Menu.MostrarMenu();
+        }
+    }
+}
